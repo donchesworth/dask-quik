30ff338fc7d12eee741d85513e48c33c2f074882
Don Chesworth <dcheswor@redhat.com>
2021-02-25 15:37:06 -0500

    added assert with final_data

--- dask_quik/tests/test_cartesian.py
+++ dask_quik/tests/test_cartesian.py
@@ -3,37 +3,59 @@ import pytest
 import pandas as pd
 import json
 from argparse import Namespace
-# from dask_quik.cartesian import sparse_cudf_matrix
+import dask.dataframe as dd
+import dask_quik.cartesian as dqcart
 
-DATA = Path.cwd().joinpath("dask_quik", "tests", "sample_data.json")
+SAMPLE = Path.cwd().joinpath("tests", "sample_data.json")
+FINAL = Path.cwd().joinpath("tests", "final_data.json")
 
 @pytest.fixture
 def cols_dict(scope="module"):
     """sample column names"""
     cols = {
         "user": "user_number",
-        "item": "item_id",
-        "product": "product_id"
+        "item": "item_id"
     }
     return cols
-
 
 @pytest.fixture
 def sample_data(scope="module"):
     """sample user/item dataset"""
-    with open(DATA) as f:
+    with open(SAMPLE) as f:
         df = pd.DataFrame(json.load(f))
+    return df
+
+@pytest.fixture
+def final_data(scope="module"):
+    """final user/item dataset"""
+    with open(FINAL) as f:
+        df = pd.DataFrame(json.load(f))
+    df.index.names = ['ui_index']
     return df
 
 
 @pytest.fixture
-def counts_dict(scope="module"):
+def counts_dict(sample_data, scope="module"):
     """unique counts for sample_data"""
     counts = {
-        "user_num": len(sample_data.iloc[0].unique()),
-        "item_num": len(sample_data.iloc[1].unique())
+        "user_number": len(sample_data.iloc[:,0].unique()),
+        "item_id": len(sample_data.iloc[:,1].unique())
     }
     return counts
+
+
+@pytest.fixture
+def colv(cols_dict, scope="module"):
+    """column values"""
+    colv = list(cols_dict.values())
+    return colv
+
+
+@pytest.fixture
+def tcol(cols_dict, scope="module"):
+    """two column value"""
+    tcol = "_".join(cols_dict.keys())
+    return tcol
 
 
 @pytest.fixture
@@ -55,7 +77,6 @@ def test_arg_gpus(gpus):
 
 def test_dask_df(sample_data, args):
     """create a dask df"""
-    import dask.dataframe as dd
     ddf = dd.from_pandas(sample_data, npartitions=args.partitions)
     assert isinstance(ddf, dd.DataFrame)
 
@@ -88,18 +109,55 @@ def test_dask_cudf(sample_data, args):
         print(e_info)
 
 
-def test_cartesian_ddf(sample_data, args):
+def test_cartesian_df(sample_data, colv, args):
     """create a dask cartesian df. If gpus, 
     output should be a dask_cudf df, else dask df"""
-    import dask.dataframe as dd
-    ddf = dd.from_pandas(sample_data, npartitions=2)
-    from dask_quik.cartesian import dask_cudf_cartesian
-    sm = dask_cudf_cartesian(ddf, ["user", "item"], args)
+    ddf = dd.from_pandas(sample_data, npartitions=args.partitions)
+    sm = dqcart.dask_cudf_cartesian(ddf, colv, args)
     if bool(args.gpus):
         import dask_cudf
         assert isinstance(sm, dask_cudf.DataFrame)
-        print("made a dask_cudf df")
+        print("made a dask_cudf cartesian df")
     else:
         assert isinstance(ddf, dd.DataFrame)
-        print("made a dask df")
-    print(sm.compute())
+        print("made a dask cartesian df")
+
+
+def test_indexized_df(sample_data, counts_dict, colv, args):
+    """create an indexed df. If gpus, 
+    output should be a dask_cudf df, else dask df"""
+    ddf = dd.from_pandas(sample_data, npartitions=args.partitions)
+    ddf = dqcart.indexize(ddf, counts_dict, colv)
+
+
+def test_indexized_cartesian(sample_data, counts_dict, colv, args):
+    """create an indexed cartesian df. If gpus, 
+    output should be a dask_cudf df, else dask df"""
+    ddf = dd.from_pandas(sample_data, npartitions=args.partitions)
+    sm = dqcart.dask_cudf_cartesian(ddf, colv, args)
+    sm = dqcart.indexize(sm, counts_dict, colv)
+
+
+def test_sparse_matrix(final_data, sample_data, counts_dict, colv, tcol, args):
+    """create an indexed cartesian df. If gpus, 
+    output should be a dask_cudf df, else dask df"""
+    ddf = dd.from_pandas(sample_data, npartitions=args.partitions)
+    sm = dqcart.dask_cudf_cartesian(ddf.copy(), colv, args)
+    sm = dqcart.indexize(sm, counts_dict, colv)
+    ddf = dqcart.indexize(ddf, counts_dict, colv)
+    ddf[tcol] = True
+    if not bool(args.gpus):
+        colv = None
+        ddf = ddf[[tcol]]
+    sm = sm.merge(
+        ddf,
+        how="left",
+        on = colv,
+        npartitions=args.partitions,
+        left_index=True,
+        right_index=True
+    )
+    sm[tcol] = sm[tcol].fillna(False)
+    sm = sm.compute()
+    assert(sm.equals(final_data))
+    print(sm.equals(final_data))
