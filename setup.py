7bec6225fc0e3a3ecea36aa6ec85a119f6b4eec0
Don Chesworth <dcheswor@redhat.com>
2021-03-01 15:34:06 -0500

    making cudf cuda optional for non-GPU installs

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from os import path
+from os import path, system
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'requirements.txt')) as f:
     all_reqs = f.read().split('\n')
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+removes = ['git+']
+if (system("nvidia-smi -L") != 0):
+    removes.extend(['cudf', 'cuda'])
+install_requires = [x.strip() for x in all_reqs if not any(y in x for y in removes)]
+
 
 setup(
     name='dask_quik',
@@ -18,3 +22,4 @@ setup(
     packages=find_packages(exclude=["test"]),
     install_requires=install_requires
 )
+
