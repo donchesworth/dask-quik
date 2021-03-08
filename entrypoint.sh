abf2da9ebdead2ff1ad6a026b18d89dfee5117ec
Don Chesworth <dcheswor@redhat.com>
2021-03-08 15:37:18 -0500

    attempt a double coverage upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 cd /opt/dq
 # pytest
-pytest --cov=/opt/dq/dask_quik --cov-config=.coveragerc --cov-report=xml
+pytest --cov=/opt/dq/dask_quik --cov-config=.coveragerc --cov-report=xml:coverage_cpu.xml
 # curl https://codecov.io/bash
