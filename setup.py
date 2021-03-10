72e1f6fa61f8634ca6a9a74a5c97965fd9ae0f19
Don Chesworth <dcheswor@redhat.com>
2021-03-10 12:31:52 -0500

    push v0.0.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.8',
-    ]
+    ],
     packages=find_packages(exclude=["tests"]),
     install_requires=install_requires,
 )
