31727c3ce1f1f273cbcd879781cac26acf0fc5e0
Don Chesworth <dcheswor@redhat.com>
2021-03-31 16:39:17 -0400

    adding bump2version config

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ README = here.joinpath("README.md").read_text()
 with open(here.joinpath("requirements.txt")) as f:
     all_reqs = f.read().split("\n")
 
+pkgversion = {}
+with open("dask_quik/_version.py") as fp:
+    exec(fp.read(), pkgversion)
+
 removes = ["git+"]
 if system("nvidia-smi -L") != 0:
     removes.extend(["cudf", "cuda"])
@@ -20,7 +24,7 @@ install_requires = [
 
 setup(
     name="dask-quik",
-    version="0.0.2",
+    version=pkgversion,
     description="function to make working in dask_cudf and dask quik-er",
     long_description=README,
     long_description_content_type="text/markdown",
