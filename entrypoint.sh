ceed72f8eb4d73ca302227f821f008ac9981beb7
donchesworth <donald.chesworth@gmail.com>
2021-04-28 19:53:28 -0400

    test to see if coverage_cpu.xml is empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 cd /opt/dq
 # pytest
 pytest --cov=/opt/dq/dask_quik --cov-config=.coveragerc --cov-report=xml:coverage_cpu.xml
+echo $(xmllint --xpath "string(//coverage/@line-rate)" coverage_cpu.xml)
 # curl https://codecov.io/bash
